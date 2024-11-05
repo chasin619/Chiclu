8133c2a1f133c1ac17a1f6f19c689d4a27da16f2
zaheer-bashir <zaheerkpr3@gmail.com>
2024-11-06 03:13:19 +0500

    Add Some colors in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,15 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/layout/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primaryGray: "#484848",
+        orange: "#e6913f",
       },
     },
   },
