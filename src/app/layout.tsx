8256f18dc6272b0d32c1eea073adad1989c982b8
zaheer-bashir <zaheerkpr3@gmail.com>
2024-11-06 03:13:42 +0500

    Remove Colors from Radix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Theme accentColor="blue" grayColor="gray">
+        <Theme>
           <Header />
           {children}
           <Footer />
