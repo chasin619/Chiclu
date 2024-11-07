5188005fbf4018ff8c8dc35af07f291f8798be4e
zaheer-bashir <zaheerkpr3@gmail.com>
2024-11-07 13:05:51 -0800

    make the footer responsive

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -6,10 +6,15 @@ import { FacebookSvg, InstagramSvg, TwitterSvg } from "@/assets/svgs";
 
 const Footer = () => {
   return (
-    <footer className="px-20 py-10 border-t border-[color(display-p3 0 0 0 / .608)]">
-      <Flex justify="between" align="center">
+    <footer className="md:px-20 xs:px-6 py-10 border-t border-[color(display-p3 0 0 0 / .608)]">
+      <Flex
+        justify="between"
+        align="center"
+        direction={{ initial: "column", sm: "row" }}
+        gap={{ initial: "6", sm: "0" }}
+      >
         <Text color="gray">&copy; {new Date().getFullYear()} Chiclu LLC</Text>
-        <nav className="w-1/2 flex justify-between items-center">
+        <nav className="md:w-1/2 xs:w-full flex justify-between items-center xs:gap-4">
           <Link href="/about">
             <Text color="gray">About</Text>
           </Link>
@@ -21,7 +26,7 @@ const Footer = () => {
           </Link>
           <Link href="/help">
             <Text color="gray">Help</Text>
-          </Link>
+          </Link> 
         </nav>
         <Flex gap="4">
           <Image src={FacebookSvg} alt="Facebook Image" />
